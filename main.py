a31c20a9ee175a135cdada70cfc9477bc2fe5cfc
khanhbaovu <baokhanh0212@gmail.com>
2021-03-23 16:03:44 +0700

    Nice

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.responses import RedirectResponse,HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_login import LoginManager
 from models import user as userModel
 
 app = FastAPI()
@@ -11,6 +12,14 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+SECRET = "secret-key"
+manager = LoginManager(SECRET,tokenUrl="/login",use_cookie=True)
+manager.cookie_name = "some-name"
+
+
+@manager.user_loader
+def user_loader(userName):
+    return userModel.getUser(userName)
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
@@ -20,16 +29,33 @@ async def read_item(request: Request):
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     userName = form.username
     password = form.password
-    user = userModel.getUser(userName)
+    user = user_loader(userName)
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect username")
     elif password != user["password"]:
         raise HTTPException(status_code=400, detail="Incorrect password")
-    return RedirectResponse(url="/login/success" + "?username=" + user["username"],status_code=status.HTTP_302_FOUND)
+    access_token = manager.create_access_token(
+        data={"sub":user['username']}
+    )
+    resp = RedirectResponse(url="/login/success" + "?username=" + user["username"],status_code=status.HTTP_302_FOUND)
+    manager.set_cookie(resp,access_token)
+    print("Error")
+    return resp
 
 @app.get("/login/success", response_class=HTMLResponse)
-async def success(request: Request, username: str):
+async def success(request: Request, username: str, token:str =Depends(manager)):
     return templates.TemplateResponse("success.html", {"request": request, "username": username.upper()})
 
-@app.get("/register")
-async def register():
+@app.get("/register", response_class=HTMLResponse)
+async def register(request : Request):
+    return templates.TemplateResponse("register.html", {"request" : request})
+
+@app.post("/register/authen")
+async def register(form : OAuth2PasswordRequestForm = Depends()):
+    userName = form.username
+    password = form.password
+    if userModel.checkUserName(userName):
+        userModel.addUser(userName, password)
+    else:
+        raise HTTPException(status_code=400, detail="UserName exists")
+    return RedirectResponse(url="/",status_code=status.HTTP_302_FOUND)
--- models/user.py
+++ models/user.py
@@ -16,3 +16,16 @@ DB = {
 def getUser(userName: str):
     user = DB.get(userName)
     return user
+
+def checkUserName(userName : str):
+    for user in DB:
+        if userName == user:
+            return False
+    return True
+
+def addUser(userName, password):
+    newUser = {
+      "username": userName,
+      "password": password
+    }
+    DB[userName] = newUser
